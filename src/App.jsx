d95a8a54a684ec450e43b0e7f7157888fc3a47c6
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-11-29 17:41:56 +0100

    installed react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+
+//css
+import "./App.css";
+
+//pages
+import { Home } from "./pages/Home";
+import { Collection } from "./pages/Collection";
+import { NotFound } from "./pages/NotFound";
+import { BookDetails } from "./pages/BookDetails";
+
+//components
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+
+
 
 function App() {
-
   return (
-    <>
-      <p>hello world</p>
-    </>
-  )
+    <div className="app ">
+      <Navbar />
+      <main className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/:id" element={<BookDetails />} />
+          <Route path="*" element={<NotFound />} />
+
+          {/* <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
