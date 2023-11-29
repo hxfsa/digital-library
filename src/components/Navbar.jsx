e8ee67b5b7b532137d9451f831753f6a0e4ca5c5
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-11-29 18:10:21 +0100

    navbar static done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+//assets
+import logo from "../assets/logos/logo.png";
+import admin from "../assets/icons/admin.svg";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar bg-primaryLight flex justify-between">
+      <Link to="/">
+        <img src={logo} alt="logo" className="h-24" />
+      </Link>
+      <div className="links-admin flex w-1/4 justify-between items-center">
+        <div className="links">
+          <ul className="flex flex-row gap-6 font-montserrrat font-semibold">
+            <li className="hover:text-primaryDark">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="hover:text-primaryDark">
+              <Link to="/collection">Collection</Link>
+            </li>
+          </ul>
+        </div>
+        <button>
+          <img src={admin} alt="admin-icon" className="h-8 px-4" />
+        </button>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryLight: "#F4F2F2",
+        primaryDark: "#9837A3",
+      },
+
+      
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        script: ["RougeScript", "sans-serif"],
+      },
+
+    },
   },
   plugins: [],
 };
