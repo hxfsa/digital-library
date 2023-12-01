f342a433b087fd671b8e38b089fd15299164447f
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-12-02 00:13:53 +0100

    fixed footer icons sizes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,24 @@ import linkedin from "../assets/icons/linkedin.svg";
 import instagram from "../assets/icons/instagram.svg";
 
 export const Footer = () => {
+  const socialMedias = [
+    { id: 1, media: "github", icon: github},
+    { id: 2, media: "linkedin", icon: linkedin},
+    { id: 3, media: "instagram", icon: instagram},
+  ];
   return (
     <footer className="footer flex justify-between w-full px-8 bg-primaryLight pt-6 items-center ">
       <div className="logo-rights flex items-end gap-4">
         <img src={logo} alt="logo" className="h-10" />
-        <span className="text-xs pb-1	"> © 2023 Books Kingdom. All Rights Reserved.</span>
+        <span className="text-xs pb-1	">
+          {" "}
+          © 2023 Books Kingdom. All Rights Reserved.
+        </span>
       </div>
       <div className="social-medias flex items-center">
-        <img src={github} alt="github icon" className="h-6" />
-        <img src={linkedin} alt="linkedin icon" className="h-8" />
-        <img src={instagram} alt="instagram icon" className="h-7" />
+        {socialMedias.map((media) => (
+        <img src={media.icon} alt={media.media} />
+        ))}
       </div>
     </footer>
   );
