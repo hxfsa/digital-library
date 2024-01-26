47dfa7c14a18d396bbcbc453e62e2db4466c3ecd
Hafsa <hafsa.aitouarraou@outlook.fr>
2024-01-26 21:04:10 +0100

    call db in collection page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,117 +1,46 @@
 import { React, useEffect, useState } from "react";
+import axios from "axios";
 
 //components
 import { MoreBooksButton } from "../components/MoreBooksButton";
 import { SearchBar } from "../components/SearchBar";
 
-//assets
-import narnia from "../assets/images/narnia.png";
-import hp2 from "../assets/images/hp2.png";
-import adcc from "../assets/images/adcc.png";
-import hp8 from "../assets/images/hp8.png";
-
 export const Collection = ({ setShowNavbarAndFooter }) => {
-  const books = [
-    {
-      id: 1,
-      title: "Le Monde de Narnia",
-      author: "C.S Lewis",
-      image: narnia,
-    },
-    {
-      id: 2,
-      title: "Harry Potter et la Chambre des Secrets",
-      author: "J.K Rowling",
-      image: hp2,
-    },
-    {
-      id: 3,
-      title: "L'appel du Coucou",
-      author: "Robert Galbraith",
-      image: adcc,
-    },
-    {
-      id: 4,
-      title: "Harry Potter et l'enfant maudit",
-      author: "J.K Rowling",
-      image: hp8,
-    },
-    {
-      id: 5,
-      title: "Charlie et la Chocolaterie",
-      author: "Roald Dahl",
-      image:
-        "https://m.media-amazon.com/images/I/81Fe-GJQDuL._AC_UF1000,1000_QL80_.jpg",
-    },
-    {
-      id: 6,
-      title: "Dragon Ball Z",
-      author: "Akiya Toriyama",
-      image:
-        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1483474406l/3685347.jpg",
-    },
-    {
-      id: 7,
-      title: "Dragon Ball GT",
-      author: "Akiya Toriyama",
-      image: "https://pictures.abebooks.com/isbn/9783551771827-fr.jpg",
-    },
-    {
-      id: 8,
-      title: "detective conan",
-      author: "Aoyama",
-      image:
-        "https://m.media-amazon.com/images/I/81p+8xivWbL._AC_UF1000,1000_QL80_.jpg",
-    },
-    {
-      id: 9,
-      title: "Titeuf",
-      author: "zep",
-      image:
-        "https://m.media-amazon.com/images/I/81jS3gh8+IL._AC_UF894,1000_QL80_.jpg",
-    },
-    {
-      id: 10,
-      title: "teufti",
-      author: "zep",
-      image:
-        "https://m.media-amazon.com/images/I/81jS3gh8+IL._AC_UF894,1000_QL80_.jpg",
-    },
-    {
-      id: 11,
-      title: "titouf",
-      author: "zep",
-      image:
-        "https://m.media-amazon.com/images/I/81jS3gh8+IL._AC_UF894,1000_QL80_.jpg",
-    },
-  ];
-  let booksFirstPart = books.slice(0, 4);
-
-  const [booksData, setBooksData] = useState(booksFirstPart);
+  const [displayedBooks, setDisplayedBooks] = useState([]);
+  const [booksData, setBooksData] = useState([]);
   const [buttonVisible, setButtonVisible] = useState(true);
-  useEffect(() => {
-    setBooksData(booksData);
-  }, [booksData]);
+  // //get the first index and the fourth to display 4 books by 4
+  const firstElementOfTheNextFour = booksData[displayedBooks.length];
+  const lastElementOfTheNextFour = booksData[displayedBooks.length + 4];
+  const firstIndexOfTheNextFour = booksData.indexOf(firstElementOfTheNextFour);
+  const lastIndexOfTheNextFour = booksData.indexOf(lastElementOfTheNextFour);
+  const nextFour = booksData.slice(
+    firstIndexOfTheNextFour,
+    lastIndexOfTheNextFour
+  );
+  const url = `http://localhost:5500/books`;
 
-  //get the first index and the fourth to display 4 books by 4
-  const firstElementOfTheNextFour = books[booksData.length];
-  const lastElementOfTheNextFour = books[booksData.length + 4];
-  const firstIndexOfTheNextFour = books.indexOf(firstElementOfTheNextFour);
-  const lastIndexOfTheNextFour = books.indexOf(lastElementOfTheNextFour);
-  const nextFour = books.slice(firstIndexOfTheNextFour, lastIndexOfTheNextFour);
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      const books = response.data;
+      setBooksData(books);
+      let booksFirstPart = books.slice(0, 4);
+      setDisplayedBooks(booksFirstPart);
+    });
+  }, []);
 
   const handleMoreBooks = () => {
     //si il reste 4 ou + livres qui n'ont pas encore été affichés alors je les affiche sinon j'affiche tous ceux qui restent
-    if (books.length - booksData.length >= 4) {
-      setBooksData([...booksData, ...nextFour]);
-    } else if (books.length - booksData.length === 0) {
-      setBooksData(booksData);
+    if (booksData.length - displayedBooks.length >= 4) {
+      setDisplayedBooks([...displayedBooks, ...nextFour]);
+    } else if (booksData.length - displayedBooks.length === 0) {
+      setDisplayedBooks(displayedBooks);
     } else {
       let arrayOfAllNext = [];
-      for (let i = firstIndexOfTheNextFour; i < books.length; i++) {
-        arrayOfAllNext.push(books[i]);
+      for (let i = firstIndexOfTheNextFour; i < booksData.length; i++) {
+        arrayOfAllNext.push(booksData[i]);
       }
-      setBooksData([...booksData, ...arrayOfAllNext]);
+      setDisplayedBooks([...displayedBooks, ...arrayOfAllNext]);
       setButtonVisible(false);
     }
   };
@@ -122,14 +51,12 @@ export const Collection = ({ setShowNavbarAndFooter }) => {
         <SearchBar />
       </div>
       <div className="books flex mt-16 bg-primaryLight">
-        {booksData.map((book) => (
-          <div className="book ease-in hover:scale-110 transition duration-200 mb-20 bg-primaryLight">
-            <img
-              src={book.image}
-              alt={book.title}
-              key={book.id}
-              className="bookImg h-80"
-            />
+        {displayedBooks.map((book) => (
+          <div
+            className="book ease-in hover:scale-110 transition duration-200 mb-20 bg-primaryLight"
+            key={book.id}
+          >
+            <img src={book.image} alt={book.title} className="bookImg h-80" />
             <div className="bookInfos text-center	flex flex-col">
               <h1 className="font-bold"> {book.title}</h1>
               <h2 className="font-semibold">{book.author}</h2>
